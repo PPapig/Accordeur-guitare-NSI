7bb2689a5636974bd6a8f955c3ed96e95f915ad9
PPapig <72268958+PPapig@users.noreply.github.com>
2022-12-29 00:28:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,5 @@
-import pyaudio
-import numpy as np
+from determine import determine_note
+from donnees import donnees
 
-def determine_note(data):
-    # Recherche de la fréquence fondamentale
-    frequency = np.argmax(data)
-    print(f"Frequency: {frequency} Hz")
-
-    # Recherche de la note correspondante dans la table de correspondance
-    notes = {
-        "E4": 329,
-        "B3": 246,
-        "G3": 196,
-        "D3": 146,
-        "A2": 110,
-        "E2": 82,
-    }
-    for note, f in notes.items():
-        if frequency > f - 10 and frequency < f + 10:
-            return note
-    return "Note non reconnue"
-
-# Ouvre un flux d'enregistrement avec Pyaudio
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
-
-# Enregistre le son pendant 1 seconde
-data = stream.read(44100)
-
-# Ferme le flux d'enregistrement
-stream.stop_stream()
-stream.close()
-p.terminate()
-
-# Transforme les données audio en un tableau Numpy
-data = np.frombuffer(data, dtype=np.int16)
-
-# Calcule la transformée de Fourier du signal
-sp = np.fft.rfft(data)
-
-# Déterminne la note jouée
-print(determine_note(sp))
+if __name__ == "__main__":
+    print(determine_note(donnees()))
