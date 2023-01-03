d5aea4d2276675a42ce5473ab193dbfabe81fc71
PPapig <72268958+PPapig@users.noreply.github.com>
2023-01-03 13:28:34 +0100

    Update donnees.py

--- donnees.py
+++ donnees.py
@@ -6,21 +6,21 @@ def donnees():
     écoute le mmicro et convertit les données en tableau numpy
     """
     # Ouvre un flux d'enregistrement avec Pyaudio
-    p = PyAudio()
-    stream = p.open(format=paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+    flux = PyAudio()
+    stream = flux.open(format=paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
 
     # Enregistre le son pendant 1 seconde
-    data = stream.read(44100)
+    donnee = stream.read(44100)
 
     # Ferme le flux d'enregistrement
     stream.stop_stream()
     stream.close()
-    p.terminate()
+    flux.terminate()
 
     # Transforme les données audio en un tableau Numpy
-    data = frombuffer(data, dtype=int16)
+    donnee = frombuffer(data, dtype=int16)
 
     # Calcule la transformée de Fourier du signal
-    sp = fft.rfft(data)
+    fourier = fft.rfft(data)
 
-    return sp
+    return fourier
